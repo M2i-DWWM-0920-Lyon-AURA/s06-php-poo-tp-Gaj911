f502e056d749a9a74816668aaa18280f89690c4a
Gaj911 <71267614+Gaj911@users.noreply.github.com>
2020-10-22 08:38:46 +0200

    Add #1

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<?php
+
+
+$databaseHandler = new PDO('mysql:host=localhost;dbname=videogames', 'root', 'root');
+
+$statement = $databaseHandler->query('SELECT * FROM `game` LIMIT 50');
+
+$games = $statement->fetchAll();
+
+
+// var_dump($games); die;
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Video games php</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" rel="stylesheet" />
 <body>
